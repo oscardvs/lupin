f72d4a40e076a93e225a1224623cf9c7e73c6fa9
Oscar Devos <o.a.e.devos@student.tudelft.nl>
2026-04-28 18:53:26 +0200

    chore(bringup): install launch and config files in lupin_bringup
    Wire setup.py to ship launch/*.launch.{py,xml} and config/*.yaml under the
    package's share/ directory so future launch files (sim, hardware, etc.)
    are discoverable by ros2 launch without further plumbing. Drop the
    launch/ .gitkeep now that the directory has real content.

--- lupin_bringup/setup.py
+++ lupin_bringup/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'lupin_bringup'
@@ -10,9 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Launch and config files will be registered here as they are added:
-        # ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        # ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/launch',
+            glob('launch/*.launch.py') + glob('launch/*.launch.xml')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
